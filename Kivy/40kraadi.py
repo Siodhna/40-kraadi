34bc422864531793052f6a71d97bb318e90303c7
Claudia Kittask <claudiakittask@gmail.com>
2016-11-29 02:29:42 +0200

    Esilehe nupp vale suurusega

--- Kivy/40kraadi.py
+++ Kivy/40kraadi.py
@@ -5,6 +5,9 @@ kivy.require("1.8.0")
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
 from kivy.lang import Builder
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import NumericProperty
+
 from kivy.uix.widget import Widget
 from kivy.graphics import Line
 from kivy.uix.image import Image
@@ -30,6 +33,7 @@ from kivy.properties import ObjectProperty
 Window.clearcolor = get_color_from_hex('#84C0C6')
 class EsimeneLehekulg(Screen):
     pass
+
 
 class MangijateValik(Screen):
     pass
