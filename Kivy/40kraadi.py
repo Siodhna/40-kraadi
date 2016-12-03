d4c9ebb1ebf381da2a6e95b8a0a2658ff0d6e170
Claudia Kittask <claudiakittask@gmail.com>
2016-12-04 00:40:14 +0200

    Failist lugemine töötab
    Süsteem töötab, kujundus on off

--- Kivy/40kraadi.py
+++ Kivy/40kraadi.py
@@ -3,11 +3,11 @@ import kivy
 kivy.require("1.8.0")
 
 from kivy.app import App
+from kivy.properties import StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import NumericProperty
-
 from kivy.uix.widget import Widget
 from kivy.graphics import Line
 from kivy.uix.image import Image
@@ -29,8 +29,9 @@ from kivy.graphics.context_instructions import Color
 from kivy.graphics import Canvas
 import random
 from kivy.properties import ObjectProperty
+from random import randint
 
-Window.clearcolor = get_color_from_hex('#84C0C6')
+Window.clearcolor = get_color_from_hex('#51555b')
 class EsimeneLehekulg(Screen):
     pass
 
@@ -39,19 +40,52 @@ class MangijateValik(Screen):
     pass
 
 class Mang(Screen):
+    uus_tegevus = StringProperty()
+    def __init__(self,**kwargs):
+        super(Mang,self).__init__(**kwargs)
+        f = open('tegevused.txt', 'r')
+        k = list(f)
+        self.uus_tegevus = str(k[randint(0,len(k)-1)])
+    def Muuda_Tegevust(self):
+        f = open('tegevused.txt', 'r')
+        k = list(f)
+
+
+        self.uus_tegevus = str(k[randint(0,len(k)-1)])
+class Blank(Screen):
     pass
 
+
+class ManguKaart(Label):
+    pass
+
+class KraadiMang(Widget):
+    kaart = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
+    player3 = ObjectProperty(None)
+
+class Abi(Screen):
+    pass
+class Seaded(Screen):
+    pass
 class ScreenManagement(ScreenManager):
     pass
 
 
-class Kaardid(Widget):
-    pass
 presentation = Builder.load_file("40kraadi.kv")
 
 class NelikummendKraadiApp(App):
+
+
     def build(self):
+
+
+
+
+
         return presentation
+
 
 
 
